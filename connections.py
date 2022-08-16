55aaca5ea9dd5e998676017def4ee9fd76056eda
DI LU <77090688+TLzzs@users.noreply.github.com>
2022-08-16 17:31:50 +1000

    connect to mongo DB and make an operation class for manipulating the database

--- connections.py
+++ connections.py
@@ -1,4 +1,7 @@
 from pymongo import MongoClient
+from flask_mail import Mail
+
+mail = Mail()
 
 client = MongoClient('mongodb+srv://cluster:ld20010828@cluster.igm4mvn.mongodb.net/?retryWrites=true')
 user_db = client.userDB
