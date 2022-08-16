0ecfc3bdb983f2ba59c7af1972b612f4b66c5b75
DI LU <77090688+TLzzs@users.noreply.github.com>
2022-08-16 17:31:50 +1000

    connect to mongo DB and make an operation class for manipulating the database

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-"""basic Flask app - demo of using a variable in a route"""
 from flask import Flask, render_template
+from DB_operation import db_operation
+from connect_db import connect_to_userDB
+from instance import User
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
+    new_user = User('DI Lu', 'dilu0828@hotmail.com', '123')
+    operation.create_user_record(new_user)
     return render_template('index.html')
 
 
@@ -15,4 +19,6 @@ def user(name):
 
 
 if __name__ == '__main__':
+    db = connect_to_userDB()
+    operation = db_operation(db)
     app.run(debug=True)
