8519878879035808afda12b92a7827c7630dd35c
DI LU <77090688+TLzzs@users.noreply.github.com>
2022-08-17 00:11:26 +1000

    register page

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from DB_operation import db_operation
 from connect_db import connect_to_userDB
 from instance import User
+from register_form import RegistrationForm
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    new_user = User('DI Lu', 'dilu0828@hotmail.com', '123')
-    operation.create_user_record(new_user)
     return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('hello.html', name=name)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST':
+        if form.validate():
+            new_user = User(form.name.data,
+                            form.email.data,
+                            form.password.data)
+            operation.create_user_record(new_user)
+        else:
+            return redirect(url_for("user", name='DI LU'))
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
