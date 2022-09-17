3e943e8c6396f4ebe237bb4f352ccdb2820bab71
troublepear <xiaohuyaw@gmail.com>
2022-09-17 23:45:49 +1000

    edit profile page - js, css

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import "../css/profile.css";
 import female1 from "../image/profile/female1.png";
 
-
 export default function Profile() {
   return (
     <div className='layout-profile'>
@@ -12,21 +11,21 @@ export default function Profile() {
       <div className='edit-container'>
         <div className='edit-left-container'>
           <ul>
-            <li><input type="text"></input></li>
-            <li><input type="text"></input></li>
-            <li><input type="text"></input></li>
-            <li><input type="text"></input></li>
+            <li><input type="text" value ="Ann.b@gmail.com"></input></li>
+            <li><input type="text" value ="Ann B"></input></li>
+            <li><input type="text" value ="Old Password"></input></li>
+            <li><input type="text" value ="New Password"></input></li>
           </ul>
         </div>
         <div className='edit-right-container'>
             <ul>
-              <li><input type="text"></input></li>
-              <li><input type="text"></input></li>
-              <li><input type="text"></input></li>
-              <li><input type="text"></input></li>
+              <li><input type="text" value ="Contact"></input></li>
+              <li><input type="text" value ="Bio"></input></li>
             </ul>
         </div>
-        <button>Save Changes</button>
+        <div className="buttonBox">
+          <button>Save Changes</button>
+        </div> 
       </div>
     </div>
     );
