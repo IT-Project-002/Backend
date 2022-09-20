9a76f1d2479fbca5133fa968c1de9a43c2da8e4d
haijiang <haijiang@student.unimelb.edu.au>
2022-09-20 13:47:05 +0800

    avatar matching done

--- frontend/src/pages/market.js
+++ frontend/src/pages/market.js
@@ -1,9 +1,12 @@
 import "../css/market.css";
-import annB from "../image/profile/AnnB.png";
+import female1 from "../image/profile/female2background.png"
+import female2 from "../image/profile/female2background.png"
+import female3 from "../image/profile/female3background.png"
+import male1 from "../image/profile/male2background.png"
+import male2 from "../image/profile/male2background.png"
+import male3 from "../image/profile/male3background.png"
+
 import item1 from "../image/cloudsFav.png";
-import item2 from "../image/CloudsPainting.png";
-import item3 from "../image/embroFav.png";
-import item4 from "../image/necklaceLanding.png";
 import background from "../image/profile-photo-bg.png";
 import AddAPhotoOutlinedIcon from '@mui/icons-material/AddAPhotoOutlined';
 
@@ -12,6 +15,8 @@ import React, { useState, useEffect } from 'react'
 export default function Market() {
     const access_token=sessionStorage.getItem('access_token')
     const [data, setData]= useState({})
+    const avatar = data['Avatar'] 
+
     useEffect(()=>{
         fetch('http://localhost:9000/user/market', {
             headers : {
@@ -36,12 +41,18 @@ export default function Market() {
                 console.log(dat)
                 setData(dat)
             })
-    },[]);
+    },[access_token]);
     return (
       <div className="layout-market" >
 
         <div className="user-avatar-container">
-            <img className="user-avatar" src={annB} alt="annB"></img>
+            { avatar==="female1" ?  (<img className="user-avatar" src={female1} alt="female1"></img>) : null}
+            { avatar==="female2" ?  (<img className="user-avatar" src={female2} alt="female2"></img>) : null}
+            { avatar==="female3" ?  (<img className="user-avatar" src={female3} alt="female3"></img>) : null}
+            { avatar==="male1" ?  (<img className="user-avatar" src={male1} alt="male1"></img>) : null}
+            { avatar==="male2" ?  (<img className="user-avatar" src={male2} alt="male2"></img>) : null}
+            { avatar==="male3" ?  (<img className="user-avatar" src={male3} alt="male3"></img>) : null}
+             {/* <img className="user-avatar" src={figure3} alt="figure"></img> */}
             <img className="user-avatar-bg" src={background} alt="background"></img>
         </div>
 
