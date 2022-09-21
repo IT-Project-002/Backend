67c1051e2355f05b0057ab66d6285acba02781c0
troublepear <xiaohuyaw@gmail.com>
2022-09-21 20:51:35 +1000

    myfave from my-fav-branch. change heart from img to icon

--- frontend/src/pages/myFav.js
+++ frontend/src/pages/myFav.js
@@ -5,27 +5,58 @@ import item1 from "../image/items/item1.png"
 import item2 from "../image/items/item2.png"
 import item3 from "../image/items/item3.png"
 import item4 from "../image/items/item4.png"
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
-export default function Like() {
+// import heart from "../icon/heart.svg"
+// import likedHeart from "../icon/likedHeart.svg"
+
+export default function MyFav() {
   return (
     <div className="layout-like">
       <div className = "content">
         <h1>My Favourite</h1>
         <div className = "items">
-          <img className="singleItem" src={item1} alt="item1" ></img>
-          <img className="singleItem" src={item2} alt="item2" ></img>
-          <img className="singleItem" src={item3} alt="item3" ></img>
-          <img className="singleItem" src={item4} alt="item4" ></img>
+        <div className="singleImg">
+          <FavoriteBorderIcon className="heart"/>
+          <img className = "ItemImg" src={item1} alt="item3" ></img>
+          <h2>Embroidery Artist Katerina Marchenko</h2>
         </div>
-        <div className = "itemsName">
-          <h2 className="singleItem">Embroidery Artist Katerina Marchenko</h2>
-          <h2 className="singleItem">Bébé mobile Crèche mobile Crèche neutre Lit bébé mobile Cloud</h2>
-          <h2 className="singleItem">Spring in Coming in my mug</h2>
-          <h2 className="singleItem">Painting: Clouds</h2>
+        <div className="singleImg">
+          <FavoriteBorderIcon className="heart"/>
+          <img className = "ItemImg" src={item2} alt="item4" ></img>
+          <h2>Bébé mobile Crèche mobile Crèche neutre Lit bébé mobile Cloud</h2>
+        </div>
+        <div className="singleImg">
+          <FavoriteBorderIcon className="heart"/>
+          <img className = "ItemImg" src={item3} alt="item5" ></img>
+          <h2>Spring in Coming in my mug</h2>
+        </div>
+        <div className="singleImg">
+          <FavoriteBorderIcon className="heart"/>
+          <img className = "ItemImg" src={item4} alt="item6" ></img>
+          <h2>Painting: Clouds</h2>
+        </div>
+        <div className="singleImg">
+          <FavoriteBorderIcon className="heart"/>
+          <img className = "ItemImg" src={item1} alt="item6" ></img>
+          <h2>Painting: Clouds</h2>
+        </div>
+        <div className="singleImg">
+          <FavoriteBorderIcon className="heart"/>
+          <img className = "ItemImg" src={item2} alt="item6" ></img>
+          <h2>Painting: Clouds</h2>
+        </div>
+        <div className="singleImg">
+          <FavoriteBorderIcon className="heart"/>
+          <img className = "ItemImg" src={item3} alt="item6" ></img>
+          <h2>Painting: Clouds</h2>
         </div>
       </div>
-      <img className="itemhead" src={backgroundTop} alt="backgroundTop"></img>
-      <img className="itemfoot" src={backgroundBottom} alt="backgroundBottom" ></img>
+
+      </div>
+      <img className="itemhead" src={backgroundTop} alt="item1" ></img>
+      <img className="itemfoot" src={backgroundBottom} alt="item2" ></img>
+
     </div>
   );
 }
