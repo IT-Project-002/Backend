7c0497a953fa7fde78b63139e55e90bc05b1159d
haijiang <haijiang@student.unimelb.edu.au>
2022-09-21 16:34:56 +0800

    added slider css

--- frontend/src/components/imageSlider.js
+++ frontend/src/components/imageSlider.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import sample from "../image/items/item1.png";
+// import sample from "../image/items/item1.png";
+import sample from "../image/background/register.png";
 import sample1 from "../image/pages/aboutUs3.png"
 import sample2 from "../image/pages/landingSlider2.png"
 import "../css/slider.css"
