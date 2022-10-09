502c8979ef51f4dee7890c7cb095fc45166a9b69
DI LU <77090688+TLzzs@users.noreply.github.com>
2022-10-09 18:44:03 +1100

    test update

--- tests/unit/test_market.py
+++ tests/unit/test_market.py
@@ -7,7 +7,7 @@ def test_market():
             "password": "Ld20010828",
         }).json['access_token']
     headers = {'Authorization': "Bearer " + token}
-    response = client.get("/users/market", headers=headers)
+    response = client.get("/users/market/ecdc86bf-ab71-478b-8ed7-478f4b563731", headers=headers)
     assert response.status_code == 200
 
 def test_market_401():
